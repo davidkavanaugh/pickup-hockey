3433568b741c746da272aec7f55b9adcf490357c
davidkavanaugh <davidkavanaugh@outlook.com>
2019-06-27 11:16:33 -0700

    add server.js

---
+++ client/src/components/Manager/ManagerLogin.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import icon from '../../images/icon.jpg';
+
+class ManagerLogin extends Component {
+    render() { 
+        return ( 
+            <div>
+                <div className="navbar">
+                    <img className="icon" src={icon} alt="icon" />
+                    <header>Manager Login</header>
+                    <span className="spacer"></span>
+                </div>
+                <div id="login">
+                    <form className="form_white-bg">
+                        
+                        <label>LOGIN</label>
+
+                        <input className="form-input" required type="text" placeholder="username" name="username" />
+                 
+                        <input className="form-input" required type="password" placeholder="password" name="password" /><br />
+                     
+                        <input className="red-btn" type="submit" value="Login" />
+                    </form>
+                    <span className="or">OR</span>
+                    <button className="red-btn">Register</button>
+                    <Link className="switch-login-link" to="/">PLAYER LOGIN</Link>
+                </div>   
+            </div>
+         );
+    }
+}
+ 
+
+
+export default ManagerLogin;
---
+++ client/src/components/Manager/ManagerUpcomingGames.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ManagerUpcomingGames extends Component {
+    render() { 
+        return ( 
+            <div>
+                <header>ManagerUpcomingGames Works</header>
+            </div>
+         );
+    }
+}
+ 
+export default ManagerUpcomingGames;
