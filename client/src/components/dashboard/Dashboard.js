3e53e2c98d33c26f0b92abc103234782fa548890
davidkavanaugh <davidkavanaugh@outlook.com>
2019-08-23 16:26:39 -0700

    dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -13,25 +13,18 @@ class Dashboard extends Component {
     const { user } = this.props.auth;
 
     return (
-      <div style={{ height: "75vh" }} className="container valign-wrapper">
+      <div className="center">
         <div className="row">
-          <div className="landing-copy col s12 center-align">
+          <div>
             <h4>
-              <b>Hey there,</b> {user.name.split(" ")[0]}
-              <p className="flow-text grey-text text-darken-1">
-                You are logged into a full-stack{" "}
-                <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
-              </p>
+              Hey there, {user.name.split(" ")[0]}
             </h4>
+            <p>
+              You are logged in
+            </p>
             <button
-              style={{
-                width: "150px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px",
-                marginTop: "1rem"
-              }}
               onClick={this.onLogoutClick}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+              className="red-btn"
             >
               Logout
             </button>
