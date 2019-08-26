62c7e40ab5bed6f8d685df5e9b520a0a3692a434
David Kavanaugh <davidkavanaugh@outlook.com>
2019-08-26 07:18:11 -0700

    cleanUp

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,26 +1,36 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 import Icon from '../../images/icon.jpg';
+import Hamburger from '../../images/hamburger.svg'
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <div className="navbar">
-        <Link 
-          to='/'
-        >
-          <img src={Icon} className='icon' alt='website icon' />
-        </Link>
-        <Link
-          to="/"
-          className="header"
-        >
-          Pickup Hockey
-        </Link>
-        <span className='spacer'></span>
-      </div>
-    );
-  }
-}
+const Navbar = ({ component: Component, auth}) => (
+  <div className="navbar">
+    {auth.isAuthenticated === true ? (
+      <Link to='/'>
+        <img src={Hamburger} className='hamburger' alt='toggle menu' />
+      </Link>
+    ) : (
+      <Link to='/'>
+        <img src={Icon} className='icon' alt='website icon' />
+      </Link>
+    )
+    }
+    <Link to='/' className='header'>
+      <h3>Pickup Hockey</h3>
+    </Link>
+    <span className='spacer'></span>
+  </div>
+);
 
-export default Navbar;
+Navbar.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(Navbar);
+
